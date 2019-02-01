903cf4f41ae9a262d6bc803cfa39c519fe429bd4
Matt Cooper <mattc@xbox.com>
2019-02-01 06:51:50 -0500

    add back a no-op unit test

---
+++ src/unittest/noop.test.ts
@@ -0,0 +1,21 @@
+/*---------------------------------------------------------------------------------------------
+*  Copyright (c) Microsoft Corporation. All rights reserved.
+*  Licensed under the MIT License.
+*--------------------------------------------------------------------------------------------*/
+
+// import * as assert from 'assert';
+
+// TODO: Implement.
+suite("No-op Tests", function () {
+
+    test('No-op test', function() {
+        // Arrange
+        
+        
+        // Act
+
+
+        // Assert
+
+    });
+});
